380b2fb7da2fc9f5fe73e8203321af8ef07853ea
Shahin <thekingmaker6x@gmail.com>
2024-11-14 11:36:06 +0100

    add new eval. and plots

--- evaluation/evaluation_test.py
+++ evaluation/evaluation_test.py
@@ -4,27 +4,50 @@ import matplotlib.patches as mpatches
 from matplotlib.lines import Line2D
 
 # Data provided
+#data = {
+#    "Max depth": [1, 3, 3, 5, 1, 3, 99, 3],
+#    "Beam width": [3, 1, 5, 3, 1, 3, 99, 2],
+#    "Number of unique entailing triples": [9, 44, 46, 54, 9, 49, 74, 43],
+#    "Total entailing triples selected for refinement": [2, 20, 19, 20, 2, 19, 30, 19],
+#    "Number of direct or indirect refinements of a goal": [1, 11, 10, 11, 1, 10, 14, 10],
+#    "Total leaf goals": [1, 5, 4, 5, 1, 4, 7, 4],
+#    "Depth of the goal hierarchy": [1, 5, 5, 5, 1, 5, 5, 5],
+    ##"Execution Time (s)": [130.91, 431.39, 632.79, 636.88, 68.76, 632.79, 2287.91, 453.83],
+#}
+
 data = {
     "Max depth": [1, 3, 3, 5, 1, 3, 99, 3],
     "Beam width": [3, 1, 5, 3, 1, 3, 99, 2],
-    "Number of unique entailing triples": [9, 44, 46, 54, 9, 49, 74, 43],
-    "Total entailing triples selected for refinement": [2, 20, 19, 20, 2, 19, 30, 19],
+    "Number of unique entailing triples": [9, 44, 46, 54, 9, 49, 75, 50],
+    "Total entailing triples selected for refinement": [2, 20, 19, 20, 2, 19, 29, 18],
     "Number of direct or indirect refinements of a goal": [1, 11, 10, 11, 1, 10, 14, 10],
     "Total leaf goals": [1, 5, 4, 5, 1, 4, 7, 4],
     "Depth of the goal hierarchy": [1, 5, 5, 5, 1, 5, 5, 5],
-    #"Execution Time (s)": [130.91, 431.39, 632.79, 636.88, 68.76, 632.79, 2287.91, 453.83],
 }
 
+#data_time_execution = {
+#    'GoalModel_MD1_BW3': [35.02, 48.27],
+#    'GoalModel_MD3_BW1': [33.53, 56.82, 43.64, 38.48, 33.97, 27.47, 57.26, 41.50, 42.93, 22.37, 27.73, 28.06],
+#    'GoalModel_MD3_BW5': [42.62, 88.29, 60.08, 64.03, 45.81, 45.53, 119.04, 40.33, 53.78, 26.77, 46.51],
+#    'GoalModel_MD5_BW3': [41.71, 93.28, 51.72, 46.89, 38.07, 36.85 , 110.58, 74.97, 39.30, 25.86, 32.81, 44.84],
+#    'GoalModel_MD1_BW1': [32.76, 36],
+#    'GoalModel_MD3_BW3': [35.93, 90.32, 51.95, 45.74, 43.63, 38.03, 90.11, 73.63, 39.05, 36.64, 39.81],
+#    'GoalModel_MD99_BW99': [71.63, 384.29, 143.93, 111.70, 100, 79.87, 94.74, 100.53, 434.39, 40.95, 206.66, 213.59, 139.61, 50.46, 115.56],
+#    'GoalModel_MD3_BW2': [42.12, 75.87, 53.26, 47.95, 40.88, 33.42, 93.80, 51.13, 34.20, 29.15, 38.71],
+#}
+
+
 data_time_execution = {
-    'GoalModel_MD1_BW3': [35.02, 48.27],
-    'GoalModel_MD3_BW1': [33.53, 56.82, 43.64, 38.48, 33.97, 27.47, 57.26, 41.50, 42.93, 22.37, 27.73, 28.06],
-    'GoalModel_MD3_BW5': [42.62, 88.29, 60.08, 64.03, 45.81, 45.53, 119.04, 40.33, 53.78, 26.77, 46.51],
-    'GoalModel_MD5_BW3': [41.71, 93.28, 51.72, 46.89, 38.07, 36.85 , 110.58, 74.97, 39.30, 25.86, 32.81, 44.84],
-    'GoalModel_MD1_BW1': [32.76, 36],
-    'GoalModel_MD3_BW3': [35.93, 90.32, 51.95, 45.74, 43.63, 38.03, 90.11, 73.63, 39.05, 36.64, 39.81],
-    'GoalModel_MD99_BW99': [71.63, 384.29, 143.93, 111.70, 100, 79.87, 94.74, 100.53, 434.39, 40.95, 206.66, 213.59, 139.61, 50.46, 115.56],
-    'GoalModel_MD3_BW2': [42.12, 75.87, 53.26, 47.95, 40.88, 33.42, 93.80, 51.13, 34.20, 29.15, 38.71],
+    'GoalModel_MD1_BW3': [24.44, 27.32],
+    'GoalModel_MD3_BW1': [23.20, 39.81, 29.57, 25.44, 26.54, 21.64, 35.70, 25.22, 26.34, 15.70, 21.26, 21.58],
+    'GoalModel_MD3_BW5': [22.79, 49.14, 29.41, 27.61, 24.81, 26.86, 43.36, 21.33, 25.98, 17.24, 27.43],
+    'GoalModel_MD5_BW3': [23.40, 48.59, 29.15, 25.46, 24.24, 24.40, 45.24, 37.46, 24.40, 19.75, 21.40, 28.44],
+    'GoalModel_MD1_BW1': [22.46, 25.24],
+    'GoalModel_MD3_BW3': [24.69, 49.99, 30.65, 28.17, 26.09, 25.74, 43.38, 40.36, 24.36, 22.15, 27.87],
+    'GoalModel_MD99_BW99': [25.57, 70.36, 35.82, 31.49, 29.06, 26.11, 30.03, 31.31, 65.14, 21.18, 43.45, 49.68, 37.88, 18.87, 33.28],
+    'GoalModel_MD3_BW2': [23.11, 48.96, 30.26, 25.28, 23.40, 22.60, 42.47, 30.69, 24.76, 22.80, 21.97],
 }
+
 
 colors = {
     1: "r",
@@ -68,7 +91,7 @@ for idx, row in df.iterrows():
     ax1.scatter(row["Mean Execution Time (s)"], row["Relevance (%)"], c=colors[row["Max depth"]], marker=markers[row["Beam width"]], s=80)
 
 ax1.set_xlabel("Mean Execution Time (s)", fontsize='large')
-ax1.set_xlim([0, 160])
+ax1.set_xlim([0, 60])
 ax1.set_ylabel("Relevance (%)", fontsize='large')
 ax1.set_ylim([0, 47])
 
@@ -90,7 +113,8 @@ bw_99_lgd = Line2D([0], [0], marker="D", ms=8, color='k', ls="", label='99')
 ax1.legend(handles=[md_title, md_1_lgd, md_3_lgd, md_5_lgd, md_99_lgd, line_skip, bw_title, bw_1_lgd, bw_2_lgd, bw_3_lgd, bw_5_lgd, bw_99_lgd], fontsize='large', loc='lower right')
 
 plt.tight_layout()
-plt.savefig("./relevance_time_tradeoff.eps")
+#plt.savefig("./relevance_time_tradeoff.eps")
+plt.savefig("./relevance_time_tradeoff_local.eps")
 plt.show()
 
 # Execution time vs NUET plot
@@ -100,7 +124,7 @@ for idx, row in df.iterrows():
     ax2.scatter(row["Mean Execution Time (s)"], row["Number of unique entailing triples"], c=colors[row["Max depth"]], marker=markers[row["Beam width"]], s=80)
 
 ax2.set_xlabel("Mean Execution Time (s)", fontsize='large')
-ax2.set_xlim([0, 160])
+ax2.set_xlim([0, 60])
 ax2.set_ylabel("Number of unique entailing triples", fontsize='large')
 ax2.set_ylim([0, 80])
 
@@ -122,5 +146,6 @@ bw_99_lgd = Line2D([0], [0], marker="D", ms=8, color='k', ls="", label='99')
 ax2.legend(handles=[md_title, md_1_lgd, md_3_lgd, md_5_lgd, md_99_lgd, line_skip, bw_title, bw_1_lgd, bw_2_lgd, bw_3_lgd, bw_5_lgd, bw_99_lgd], fontsize='large')
 
 plt.tight_layout()
-plt.savefig("./nuet_time_tradeoff.eps")
+#plt.savefig("./nuet_time_tradeoff.eps")
+plt.savefig("./nuet_time_tradeoff_local.eps")
 plt.show()
